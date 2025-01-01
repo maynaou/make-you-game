159df2d376c098eb687a909b420a36fbf450af76
Your Name <you@example.com>
2025-01-01 16:37:03 +0100

    make-your-game

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ class Paddle {
         this.color = color;
         this.element = document.createElement('div');
         this.element.className = 'paddle';
-        this.element.style.width = '110px';
+        this.element.style.width = '130px';
         this.element.style.height = '20px';
-        this.element.style.backgroundColor = color;
+        // this.element.style.backgroundColor = color;
         this.element.style.position = 'absolute';
         document.getElementById('border-container').appendChild(this.element);
         this.center();
@@ -47,8 +47,6 @@ class Paddle {
     }
 }
 
-
-
 class Ball {
     constructor(x, y, speed, color, loselife) {
         this.x = x;
@@ -56,7 +54,7 @@ class Ball {
         this.speed = speed;
         this.dx = speed;
         this.dy = -speed;
-        this.radius = 10;
+        this.radius = 12;
         this.color = color;
         this.loselife = loselife;
         this.element = document.createElement('div');
@@ -117,8 +115,7 @@ class Ball {
             const paddleCenter = paddle.offsetLeft + paddle.offsetWidth / 2;
             const hitPosition = (this.x + this.radius) - paddleCenter;
             const normalizedHit = hitPosition / (paddle.offsetWidth / 2);
-            this.dx = normalizedHit * this.speed;
-            console.log(hitPosition, normalizedHit, paddleCenter)
+            this.dx = normalizedHit * this.speed;  
         }
     }
 
@@ -131,14 +128,14 @@ class Ball {
 }
 
 class ScoreBoard {
-    constructor(Time, score, lives,timerInterval) {
+    constructor(Time, score, lives, timerInterval) {
         this.Time = Time;
         this.score = score;
         this.lives = lives;
         this.timerInterval = timerInterval;
         this.element = document.createElement('div');
         this.element.id = 'scoreboard';
-        this.element.style.display = 'flex'; 
+        this.element.style.display = 'flex';
         this.element.style.justifyContent = 'space-around';
         this.element.style.alignItems = 'center';
         this.element.style.backgroundColor = '#333';
@@ -147,7 +144,6 @@ class ScoreBoard {
         this.element.style.fontSize = '20px';
         this.element1 = document.createElement('div');
         this.element1.id = 'timer';
-        this.element1.textContent = Time;
         this.element2 = document.createElement('div');
         this.element2.id = 'score';
         this.element2.textContent = `Score: ${score}`;
@@ -159,19 +155,16 @@ class ScoreBoard {
         this.element.appendChild(this.element3);
         document.body.appendChild(this.element);
     }
-    
+
     startTimer() {
         this.timerInterval = setInterval(() => {
-            if (this.Time > 0) {
-                this.Time--;
+            if (!isPaused) {
+                this.Time++;
                 const minutes = Math.floor(this.Time / 60);
                 const seconds = this.Time % 60;
-                document.getElementById('timer').textContent =`${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`; 
-            }else {
-                clearInterval(timerInterval);
-                alert("Le temps est écoulé!");
+                document.getElementById('timer').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
             }
-        },1000)
+        }, 1000)
     }
 
     updateScore() {
@@ -179,7 +172,7 @@ class ScoreBoard {
     }
 
     updateLives() {
-        this.element3.textContent = `Lives: ${lives}`; 
+        this.element3.textContent = `Lives: ${lives}`;
     }
 }
 
@@ -187,7 +180,7 @@ let timerInterval;
 
 let lives = 3
 
-let scoreboard = new ScoreBoard(60, 0,lives,timerInterval);
+let scoreboard = new ScoreBoard(0, 0, lives, timerInterval);
 
 class MenuPause {
     constructor(color, isSpacePressed) {
@@ -199,14 +192,15 @@ class MenuPause {
         this.element.style.position = 'absolute';
         this.element.style.top = '50%';
         this.element.style.left = '50%';
-        this.element.style.transform = 'translate(-127%, -50%)';
+        this.element.style.transform = 'translate(-50%, -50%)';
         this.element.style.background = 'rgba(0, 0, 0, 0.8)';
         this.element.style.color = color;
         this.element.style.padding = '20px';
         this.element.style.borderRadius = '10px';
         this.element.style.textAlign = 'center';
+        this.element.style.zIndex = 1;
         this.createButtons();
-        document.body.appendChild(this.element);
+        document.getElementById('border-container').appendChild(this.element);
     }
 
     createButtons() {
@@ -241,15 +235,25 @@ class MenuPause {
 }
 
 const menuPause = new MenuPause('white', true);
-const paddle = new Paddle(30, "white");
-const ball = new Ball(paddle.x + (paddle.element.offsetWidth / 2) - 10, paddle.y - 20, 5, "red", loselife);
+const paddle = new Paddle(30, "#ffcc00");
+const ball = new Ball(paddle.x + (paddle.element.offsetWidth / 2) - 10, paddle.y - 20, 7, '#ff0000', loselife);
 
 const brickMatrix = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 1, 2, 1, 2, 0, 0, 0],
+    [0, 0, 3, 0, 2, 0, 2, 0, 3, 0, 0],
+    [0, 0, 4, 3, 1, 3, 1, 3, 4, 0, 0],
+    [0, 5, 2, 1, 0, 5, 0, 1, 2, 5, 0],
+    [2, 0, 0, 0, 6, 0, 6, 0, 0, 0, 2],
+    [0, 0, 0, 7, 1, 6, 1, 7, 0, 0, 0],
+    [0, 0, 8, 1, 7, 0, 7, 1, 8, 0, 0],
+    [0, 9, 2, 0, 1, 9, 1, 0, 2, 9, 0],
+    [10, 0, 0, 0, 3, 0, 3, 0, 0, 0, 10],
+    [0, 0, 0, 2, 0, 1, 0, 2, 0, 0, 0],
+    [0, 0, 4, 3, 5, 4, 5, 3, 4, 0, 0],
+    [0, 4, 0, 5, 4, 1, 4, 5, 0, 4, 0],
+    [0, 7, 6, 1, 0, 4, 0, 1, 6, 7, 0],
+    [1, 0, 0, 0, 0, 10, 0, 0, 0, 0, 1],
 ];
 
 const bricks = []
@@ -268,7 +272,7 @@ class Brick {
         this.element.style.position = 'absolute ';
         this.element.style.width = `${width}px`;
         this.element.style.height = `${height}px`;
-        this.element.style.backgroundColor = color;
+        this.element.style.background = `linear-gradient(135deg,${color},#69665a`;
         this.element.style.left = `${x}px`;
         this.element.style.top = `${y}px`;
         document.getElementById('border-container').appendChild(this.element);
@@ -309,7 +313,7 @@ class Brick {
                     ball.bouceVertical()
                 }
                 this.visible = false;
-                score +=10 
+                score += 10
                 scoreboard.updateScore()
             }
         }
@@ -319,18 +323,30 @@ class Brick {
 let score = 0
 
 
-const brickWidth = 80.3;
-const brickHeight = 60;
-const brickColor = 'blue';
-const spacing = 5.4;
+const brickWidth = 84.27 ;
+const brickHeight = 50;
+const spacing = 5  ;
+const COLOR_MAP = {
+    1: "255, 255, 255", 
+    2: "255, 165, 0", 
+    3: "0, 191, 255",   
+    4: "50, 205, 50",  
+    5: "255, 0, 0",   
+    6: "30, 144, 255",  
+    7: "255, 20, 147", 
+    8: "255, 215, 0",  
+    9: "169, 169, 169",  
+    10: "255, 255, 0"  
+};
 
 function createBricksFromMatrix(matrix) {
-    for (let row = 0; row < matrix.length; row++) {
+    for (let row = 0 ; row < matrix.length; row++) {
         for (let col = 0; col < matrix[row].length; col++) {
-            if (matrix[row][col] !== 0) {
-                const x = col * (brickWidth + spacing);
+            const brickValue = matrix[row][col]
+            if (brickValue > 0) {
+                const x = col * (brickWidth + spacing) + 2;
                 const y = row * (brickHeight + spacing);
-                const brick = new Brick(x, y, brickWidth, brickHeight, brickColor)
+                const brick = new Brick(x, y, brickWidth, brickHeight,`rgb(${COLOR_MAP[brickValue]})`)
                 bricks.push(brick)
             }
         }
